664e70db1937a2be5508d6925bbb79932f4ba4e6
Sekyom <35869054+Sekyom@users.noreply.github.com>
2018-09-29 02:17:02 +0200

    Update ft_putnbr.c

--- d05/ex01/ft_putnbr.c
+++ d05/ex01/ft_putnbr.c
@@ -17,18 +17,24 @@ void	ft_putnbr(int nb)
 	long nbr;
 
 	nbr = nb;
-	if (nb < 0)
+	if (nb = "-2147483648")
 	{
 		ft_putchar('-');
-		nb = nb * (-1);
+		nbr = "21474"
+		nb = "83648";
 	}
-	if (nb >= 10)
+	if (nb < 0)
 	{
-		ft_putnbr(nb / 10);
-		ft_putchar(nb % 10);
+		ft_putchar("-") 
+		nb = nb * "-1";
 	}
 	if (nb <= 9)
 	{
 		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10);
 	}
 }
